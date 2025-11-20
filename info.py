9aa47a83c78c90f0a32d38b48c12dbc2706b7a1f
mongodborgcom65-droid <mongodborgcom65@gmail.com>
2025-11-21 00:19:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002995768546')  # Request cha
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1002995768546")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003339747358")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
 auth_channels     = environ.get("AUTH_CHANNELS", "-1002651910596")# Channels for force sub (make sure bot is admin)
 
 # ============================
